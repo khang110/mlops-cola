fc665c793acd0a6add29f83243cb5e7192212baa
khang110 <anhkhang110@gmail.com>
2026-05-25 15:03:42 +0800

    refactor: implement lazy loading for model inference instance and update build dependencies and configuration

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,17 @@ ENV LANG=C.UTF-8
 ENV LC_ALL=C.UTF-8
 ENV PYTHONIOENCODING=utf-8
 ENV HF_HOME=/tmp/huggingface
-ENV TRANSFORMERS_CACHE=/tmp/huggingface
 
 COPY ./ ${LAMBDA_TASK_ROOT}
 WORKDIR ${LAMBDA_TASK_ROOT}
 
+RUN pip install --upgrade pip
 RUN pip install pysqlite3-binary
+
 RUN pip install -r requirements_inference.txt
 
 RUN python3 -c "import site,os; sc=os.path.join(site.getsitepackages()[0],'sitecustomize.py'); open(sc,'w').write('import sys\ntry:\n    import pysqlite3\n    sys.modules[\"sqlite3\"]=pysqlite3\nexcept Exception:\n    pass\n')"
 
-RUN python lambda_handler.py
+# RUN python lambda_handler.py
 # RUN chmod -R 0755 $MODEL_DIR
 CMD [ "lambda_handler.lambda_handler"]
--- lambda_handler.py
+++ lambda_handler.py
@@ -5,25 +5,32 @@ Lambda wrapper
 import json
 from inference_onnx import ColaONNXPredictor
 
-inferencing_instance = ColaONNXPredictor("./models/model.onnx")
+inferencing_instance = None
+
+def get_inferencing_instance():
+    global inferencing_instance
+    if inferencing_instance is None:
+        inferencing_instance = ColaONNXPredictor("./models/model.onnx")
+    return inferencing_instance
 
 def lambda_handler(event, context):
-	"""
-	Lambda function handler for predicting linguistic acceptability of the given sentence
-	"""
-	
-	if "resource" in event.keys():
-		body = event["body"]
-		body = json.loads(body)
-		print(f"Got the input: {body['sentence']}")
-		response = inferencing_instance.predict(body["sentence"])
-		return {
-			"statusCode": 200,
-			"headers": {},
-			"body": json.dumps(response)
-		}
-	else:
-		return inferencing_instance.predict(event["sentence"])
+    """
+    Lambda function handler for predicting linguistic acceptability of the given sentence
+    """
+    instance = get_inferencing_instance()
+    
+    if "resource" in event.keys():
+        body = event["body"]
+        body = json.loads(body)
+        print(f"Got the input: {body['sentence']}")
+        response = instance.predict(body["sentence"])
+        return {
+            "statusCode": 200,
+            "headers": {},
+            "body": json.dumps(response)
+        }
+    else:
+        return instance.predict(event["sentence"])
 
 if __name__ == "__main__":
 	test = {"sentence": "this is a sample sentence"}
